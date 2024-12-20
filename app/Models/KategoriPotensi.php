099d875d818c3ad778c8b42d3a94189c398963f3
TurnNull <husain.7605@gmail.com>
2024-12-20 10:16:00 +0800

    awalan

---
+++ app/Models/KategoriPotensi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriPotensi extends Model
+{
+    /** @use HasFactory<\Database\Factories\KategoriPotensiFactory> */
+    use HasFactory;
+}
